dbb34eaed97c3fd486d01dbc0fc33291b3b7dd29
kschwende <karl.schwende@gmail.com>
2024-03-07 19:45:35 -0500

    added more things, post a review not working

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -10,28 +10,29 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 class CarMake(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
-    website = models.URLField()
+    # Other fields as needed
 
     def __str__(self):
-        return self.name
+        return self.name  # Return the name as the string representation
 
 class CarModel(models.Model):
-    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
+    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)  # Many-to-One relationship
     name = models.CharField(max_length=100)
     CAR_TYPES = [
         ('SEDAN', 'Sedan'),
         ('SUV', 'SUV'),
         ('WAGON', 'Wagon'),
         ('TRUCK', 'Truck'),
-        # Add more as needed
+        # Add more choices as required
     ]
     type = models.CharField(max_length=10, choices=CAR_TYPES, default='SUV')
-    year = models.IntegerField(default=2024,
+    year = models.IntegerField(default=2023,
         validators=[
             MaxValueValidator(2025),
             MinValueValidator(2015)
         ])
+    # Other fields as needed
 
     def __str__(self):
-        return self.name
+        return self.name  # Return the name as the string representation
 
